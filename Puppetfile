733c36fcbc75d886e606ff36d34414ab7fc9104d
Nicklaus McClendon <nicklaus@kulinacs.com>
2018-09-05 10:33:52 -0500

    Remove puppet-agent module

--- Puppetfile
+++ Puppetfile
@@ -3,5 +3,3 @@ forge 'forge.puppetlabs.com'
 # Forge Modules
 mod 'puppetlabs-ntp', '4.1.0'
 mod 'puppetlabs-stdlib'
-mod 'puppetlabs-puppet_agent', '1.6.2'
-mod 'puppetlabs-inifile', '1.2.0'
--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -1,12 +1,12 @@
 class profile::base {
   class { '::ntp': }
 
-  class {'::puppet_agent':
-    package_version => '5.5.6',
-    manage_repo     => false,
-  }
-
   package { 'zsh':
     ensure => 'installed',
+  }
+
+  service { 'puppet':
+    ensure => 'running',
+    enable => 'true',
   }
 }
